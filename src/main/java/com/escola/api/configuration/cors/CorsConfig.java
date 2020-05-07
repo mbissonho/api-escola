04d908b39ccf4461ad0cc1978d3580c0746c0b40
Mateus A. Bissonho <mateus_bissonho@live.com>
2020-05-06 21:23:29 -0300

    Adicionados Access Control Allow Headers: Cache-Control e Pragma

--- src/main/java/com/escola/api/configuration/cors/CorsConfig.java
+++ src/main/java/com/escola/api/configuration/cors/CorsConfig.java
@@ -35,8 +35,9 @@ public class CorsConfig implements Filter{
         response.setHeader("Access-Control-Allow-Credentials", "true");
 		
 		if ("OPTIONS".equals(request.getMethod()) && this.property.getOriginAllowed().equals(request.getHeader("Origin"))) {
+
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
-        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, Cache-Control, Pragma");
         	response.setHeader("Access-Control-Max-Age", "3600");
 			
 			response.setStatus(HttpServletResponse.SC_OK);
